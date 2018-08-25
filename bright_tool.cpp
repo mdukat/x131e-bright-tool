dbc410292f2c8dc5b4b13615a2e80f29a09b1989
Mateusz Dukat <d3s.prv@mail.ru>
2018-08-25 14:50:50 +0200

    Refactored, still need testing

--- bright_tool.cpp
+++ bright_tool.cpp
@@ -1,59 +1,55 @@
 #include <iostream>
 #include <fstream>
-#include <string>
+#include <unistd.h>
 #include <string.h>
+
 #define brightness_file "/sys/devices/pci0000:00/0000:00:02.0/drm/card0/card0-LVDS-1/intel_backlight/brightness"
+#define uint unsigned int
 
-int some_math(int percent);
-void brightness_set(int percent);
-void brightness_increment(int percent);
-void brightness_decrement(int percent);
-void help();
-bool check_root();
-void r_not_root();
-int get_value();
-void set_value(int value);
-void r_too_high();
-void r_too_low();
+uint percentToBrightness(uint percent);
+void brightnessSet(uint percent);
+void brightnessIncrement(uint percent);
+void brightnessDecrement(uint percent);
 
-int some_math(int percent){
-	percent = 43*percent;
-	return percent;
+void checkRoot();
+uint getValue();
+void setValue(uint value);
+void raiseError(char* value);
+void raiseHelp();
+void checkPercentageCorrectness(uint percent);
+
+uint percentToBrightness(uint percent){
+	return 43*percent;
 }
 
-void brightness_set(int percent){
-	set_value(some_math(percent));
-	std::cout << "Screen brightness set to " << percent << "% (" << some_math(percent) << ")\n";
+void brightnessSet(uint percent){
+	checkPercentageCorrectness(percent);
+	setValue(percentToBrightness(percent));
 }
 
-void brightness_increment(int percent){
-	int last_state = get_value();
-	if(last_state > 4300){
-		r_too_high();
+void brightnessIncrement(uint percent){
+	checkPercentageCorrectness(percent);
+	uint last_state = getValue();
+	uint new_state = last_state + percentToBrightness(percent);
+	if(new_state > 4300){
+		throw 3;
 	}else{
-		int new_state = last_state + some_math(percent);
-		set_value(new_state);
+		setValue(new_state);
 	}
 }
 
-void brightness_decrement(int percent){
-	int last_state = get_value();
-	if(last_state <= 0){
-		r_too_low();
+void brightnessDecrement(uint percent){
+	checkPercentageCorrectness(percent);
+	uint last_state = getValue();
+	int new_state = last_state - percentToBrightness(percent);
+	if(new_state < 0){
+		throw 3;
 	}else{
-		int new_state = last_state - some_math(percent);
-		set_value(new_state);
+		setValue(new_state);
 	}
 }
 
-void set_value(int value){
-	std::ofstream b_file;
-	b_file.open(brightness_file, std::ios::out);
-	b_file << value;
-	b_file.close();
-}
-
-int get_value(){
+uint getValue(){
 	std::ifstream b_file;
 	b_file.open(brightness_file, std::ios::in);
 	std::string buffer;
@@ -64,52 +60,69 @@ int get_value(){
 	return int_buffer;
 }
 
-bool check_root(){
+void setValue(uint value){
 	std::ofstream b_file;
-	b_file.open(brightness_file, std::ios::out);
-	if(b_file){
-		b_file.close();
-		return true;
-	}else{
-		r_not_root();
-		return false;
+	b_file.open(brightness_file, std::ios::in);
+	b_file << value;
+	b_file.close();
+}
+
+void checkRoot(){
+	if(getuid() != 0){
+		throw 0;
 	}
 }
 
-void help(){
-	std::cout << "bright_tool - X131e brightness tool by d3s\nUsage: bright_tool <command> <value>\n\nCommands:\n-i <0-100> - increments brightness by %\n-d <0-100> - decrements brightness by %\n-h - shows this help\n\nif no command is given, brigh_tool will set brightness to %\n\n";
+void checkPercentageCorrectness(uint percent){
+	if(percent < 0){
+		throw 2;
+	}else if(percent > 100){
+		throw 3;
+	}
 }
 
-void r_not_root(){
-	std::cout << "Error: you are not root :/\n";
+void raiseError(char* value){
+	std::cout << "ERROR: " << value << "\n";
 }
 
-void r_too_high(){
-	std::cout << "Error: value too high\n";
+void raiseHelp(){
+	std::cout << "bright_tool - X131e brightness tool by d3s\nUsage: bright_tool <command> <value>\n\nCommands:\n-i <0-100> - increments brightness by %\n-d <0-100> - decrements brightness by %\n-s <0-100> - sets brightness to %\n-h - shows this help\n\n";
 }
 
-void r_too_low(){
-	std::cout << "Error: value too low\n";
-}
-
-int main(int argc, const char * argv[]){
-	if(argv[1] == NULL){
-		help();
-	}else{
-	if(check_root() == false){
-		return 1;
-	}else if(argv[2] == NULL){
-		if(strcmp(argv[1], "-h") == 0){
-			help();
-		}else{
-			brightness_set(atoi(argv[1]));
+int main(int argc, char** argv){
+	try{
+	checkRoot();
+	int c;
+	while((c = getopt(argc, argv, "i:d:hs:")) != -1){
+		switch(c){
+			case 'i':
+				brightnessIncrement(atoi(optarg));
+				break;
+			case 'd':
+				brightnessDecrement(atoi(optarg));
+				break;
+			case 'h':
+				throw 1;
+				break;
+			case 's':
+				brightnessSet(atoi(optarg));
+				break;
+			case '?':
+				throw 1;
+				break;
+			default:
+				abort();
 		}
-	}else{
-		if(strcmp(argv[1], "-i") == 0){
-			brightness_increment(atoi(argv[2]));
-		}else if(strcmp(argv[1], "-d") == 0){
-			brightness_decrement(atoi(argv[2]));
-		}
 	}
+	}catch(int errnum){
+		if(errnum == 0)
+			raiseError("You are not root!");
+		if(errnum == 1)
+			raiseHelp();
+		if(errnum == 2)
+			raiseError("Value too low!");
+		if(errnum == 3)
+			raiseError("Value too high!");
 	}
+	return 0;
 }
